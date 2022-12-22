80103ee4a0ce928d2f1355387450b9fd50205c47
Erikka Polk <erikka.pokl@gmail.com>
2022-12-22 14:44:50 -0600

    added ajax request to update ratings

--- server.py
+++ server.py
@@ -14,20 +14,9 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     return render_template('homepage.html') #has form w/ variable names =uname, pword
 
-@app.route('/', methods=["POST"])
-def login():
-    email=request.form.get("email")
-    password=request.form.get("password")
-    user=crud.get_user_by_email(email)
-    if user.password==password:
-        if user:
-            flash(f"Logged in!")
-            session["current_user"]=user.user_id
-            return redirect(f'/users/<user_id>') #has form w/ variable names =uname, pword
-    else:
-        flash("Please try again.")
-        return redirect("/")
-
+#######################################################
+#######################################################
+#######################################################
 
 @app.route('/movies')
 def all_movies():
@@ -38,16 +27,22 @@ def all_movies():
 def show_movie(movie_id):
     """Show details on a particular movie."""
     movie = crud.get_movie_by_id(movie_id)
-    session["movie_id"]=movie.movie_id
+    session["movie_id"]=movie.movie_id #?????????????????????????????
     return render_template("movie_details.html", movie=movie)
+
+#######################################################
+#######################################################
+#######################################################
 
 @app.route("/users")
 def show_users(): #DONE, for server
+    """show users"""
     users=crud.get_users()
     return render_template("users.html", users=users)
 
 @app.route("/users", methods=['POST'])
 def account_creation(): #DONE, for server
+    """create user"""
     email=request.form.get('email')
     password=request.form.get('password')
 
@@ -60,33 +55,89 @@ def account_creation(): #DONE, for server
         db.session.add(new_user)
         db.session.commit()
     return redirect("/")
-        
-@app.route("/users/<user_id>/<rating_id>", methods=["POST"])
+
+@app.route("/users/<user_id>")
 def show_profile(user_id): #DONE, for server
+    """deets on particular user"""
     user=crud.get_user_by_id(user_id)####
-    # answer=request.form("favorited")
-    # favorite=crud.add_fave(answer)
-    # if favorite.:
-    #     do this
-    movie_id=session["movie_id"]
-    score=request.form("rating")
-    rating=crud.create_rating(score)
-
-    #on the movie detail page, you click forms 
-    # button, route that score to favoirte 
-    # route on profile
-
-    #you take score from form and put it in a 
-    # crud.rate_movie(score) and that gives u the id
-    
-    #user from session somehow
-
-    #movie from movie detail page, thats y i want to put session on that page
-
-
     return render_template("user-profile.html", user=user)
 
-# update_rating(rating_id, new_rating)
+
+@app.route('/', methods=["POST"])
+def login():
+    """user login"""
+    email=request.form.get("email")
+    password=request.form.get("password")
+    user=crud.get_user_by_email(email)
+    if user.password==password: 
+        if user:
+            flash(f"Logged in!")
+            session["current_email"]=user.email #logged in if session is in route
+            flash(f"Welcome back, {user.email}!")
+    else: #    if not user or user.password != password:
+        flash("Please try again.")
+    return redirect("/")
+
+
+#######################################################
+#######################################################
+#######################################################
+
+
+#####needed the most help here, spent about 3 hours total 
+#figure out where input is coming from in html! either user profile or movie deets
+@app.route("/update_rating", methods=["POST"])
+def update_rating():
+    """where ratings are updated"""
+    rating_id = request.json["rating_id"] #?
+    #AJAX
+    #incorrect:rating=crud.update_rating(new_rating, rating.id)
+
+    updated_score = request.json["updated_score"]
+    #AJAX: i was wondering where to get it from, this is ajax versus form 
+
+    crud.update_rating(rating_id, updated_score)
+    #incorrect/didnt need to be in a variable:         rating=crud.update_rating(new_rating, rating.id)
+    
+    db.session.commit()
+    return "Success"
+
+
+
+@app.route("/movies/<movie_id>/ratings", methods=["POST"])
+def create_ratings(movie_id): #DONE, for server
+    """create movie rating on movie page"""
+    #INCORRECT: return render_template("user-profile.html", rating=rating)
+    
+    logged_in_email = session.get("current_email")
+    #INCORRECT:  user=session["current_user"] they logged in via email
+
+    rating_score = request.form.get("rating")
+    #this form is in the movie deets html
+
+    if logged_in_email is None:
+        flash("You must log in to rate a movie.")
+    elif not rating_score: #if theres no score, see if required or not
+        flash("Error: you didn't select a score for your rating.")
+    else:
+        user = crud.get_user_by_email(logged_in_email)
+        #^refer to session to see whos logged in
+        
+        movie = crud.get_movie_by_id(movie_id)
+        #^get movie id from parameters
+
+        rating = crud.create_rating(user, movie, int(rating_score))
+        #^create rating
+
+        db.session.add(rating)
+        db.session.commit()
+
+        flash(f"You rated this movie {rating_score} out of 5.")
+
+    return redirect(f"/movies/{movie_id}")
+
+
+
 
 
 
